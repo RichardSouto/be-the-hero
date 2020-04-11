8279ddb8bce357d0632e8b2c6032ae751569b21e
Richard Souto <richard.ssouto@gmail.com>
2020-04-11 01:17:21 -0300

    Incluindo estrutura de login.

---
+++ mobile/src/pages/Login/index.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { View, Text, Button, TouchableOpacity, AsyncStorage, Alert } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+import api from '../../../src/services/api';
+
+
+export default function Login() {
+    const [login, setLogin] = useState('');
+
+    const navigation = useNavigation();
+
+    let ongName;
+
+    async function handleLogin(){
+
+        if (login.trim() == '') {
+            Alert.alert(
+                'Be The Heroe',
+                'Deseja seguir informar o login?',
+                [
+                    {text: 'Sim', onPress: () => navigation.navigate('Incidents')},
+                    {text: 'Não', onPress: () => { return }}
+                ],
+                {onDismiss: () => { return } }
+            )
+        }
+        else
+        {
+            try {
+                ongName =  await validarLogin(login);
+    
+                AsyncStorage.setItem('ongId', login);
+                AsyncStorage.setItem('ongName', ongName);
+
+                navigation.navigate('Incidents');
+
+    
+            } catch (error) {
+                alert('Falha no Login. Tente Novamente');
+            }
+        }
+
+
+    }
+
+    async function validarLogin(login){
+        try {
+            const response = await api.post('sessions', { id: login });
+
+            return response.data.name;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.loginGroup}>
+                <Text style={styles.textLogin}>
+                    Login
+                </Text>
+
+                <View style={styles.camposLogin}>
+                    <TextInput 
+                        style={styles.loginInput}
+                        placeholder='Digite seu Ong ID'
+                        onChangeText={text => setLogin(text)} />
+                    <Button 
+                        color="#e02041"
+                        title="Entrar" 
+                        onPress={handleLogin}/>
+                </View>
+                <View style={styles.semCadastroGroup}>
+                    <TouchableOpacity 
+                        style={styles.semCadastroTouchable}
+                        onPress={() => {}}
+                    >
+                        {/* <Feather name="log-in" size={15} /> */}
+                        <Text style={styles.semCadastroText}>Cadastrar-se</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.logarSemCadastroGroup}>
+                    <Text style={styles.logarSemCadastroText}>Para logar sem cadastro, basta seguir sem preencher o campo de login.</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
+import Login from './pages/Login';
 import Incidents from './pages/Incidents';
 import Detail from './pages/Detail';
 
@@ -11,6 +12,7 @@ export default function Routes(){
     return(
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown: false}}>
+                <AppStack.Screen name="Login" component={Login} />
                 <AppStack.Screen name="Incidents" component={Incidents} />
                 <AppStack.Screen name="Detail" component={Detail} />
             </AppStack.Navigator>
