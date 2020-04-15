5e9d52c405aee13778fd4676cab4ec5e538f1976
Richard Souto <richard.ssouto@gmail.com>
2020-04-14 21:04:12 -0300

    Removendo validacao duplicada - name

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -21,7 +21,6 @@ routes.post('/ongs', celebrate({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
         whatsapp: Joi.string().required().min(10).max(11),
-        name: Joi.string().required(),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2)
     })
